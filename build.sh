6cf4b46dd77ba8d78f0e177f2ae607026ddbb1ab
Abreham Atlaw <abreham.atlaw@yahoo.com>
2024-03-29 11:06:24 +0300

    Loading resource on build

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@ pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
 python manage.py createsuperuser --no-input
+wget "https://www.dropbox.com/scl/fi/m3gjmbvif1fz3vg5bz9lm/AUD-USD.csv?rlkey=piod5flcvreghuoa7k3plfp5n&dl=0&raw=1" -O "res/data/AUD-USD-downloaded.csv"
