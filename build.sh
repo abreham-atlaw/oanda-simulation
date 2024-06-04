a924de59d76fc24f61cb2fe4573142b70ec79ff3
Abreham Atlaw <abreham.atlaw@yahoo.com>
2024-06-04 10:10:11 +0300

    Change of url

--- build.sh
+++ build.sh
@@ -3,4 +3,6 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py createsuperuser --no-input
 mkdir -p res/data/
-wget "https://docs.google.com/uc?export=download&id=18BFdUu1fJCI6VmMnq_XLYwDvhdSBAEWw" -O "res/data/AUD-USD-downloaded.csv"
+wget "https://drive.usercontent.google.com/download?id=18BFdUu1fJCI6VmMnq_XLYwDvhdSBAEWw&export=download&confirm=t&uuid=2bcd0c1a-44d3-4a7f-b647-9db8ac78b40d" -O "res/data/AUD-USD.zip"
+unzip "res/data/AUD-USD.zip" -d "res/data/"
+
