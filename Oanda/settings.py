35ec2ccd1f6b98157241419cd04f62886771185f
Abreham Atlaw <abreham.atlaw@yahoo.com>
2024-09-02 11:03:52 +0300

    stats dump path fix

--- Oanda/settings.py
+++ Oanda/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+from datetime import datetime
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -149,4 +149,4 @@ LOCAL_DEFAULT_ACCOUNT_ENV_KEY = "OANDA_LOCAL_DEFAULT_ACCOUNT"
 PRICE_CACHING_TIMEOUT = 30
 MIN_GRANULARITY = 1
 
-STATS_DUMP_PATH = RES_PATH / "stats"
+STATS_DUMP_PATH = RES_PATH / "stats" / f"{datetime.now().strftime('%Y-%m-%d')}.json"
