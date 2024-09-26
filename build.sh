f5e1cc12f502fa23d3dc8bf81c48429be2281b96
Abreham Atlaw <abreham.atlaw@yahoo.com>
2024-09-26 08:42:26 +0300

    New URL

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ python manage.py migrate
 python manage.py createsuperuser --no-input
 mkdir -p res/data/
 #wget "https://drive.usercontent.google.com/download?id=18BFdUu1fJCI6VmMnq_XLYwDvhdSBAEWw&export=download&confirm=t&uuid=2bcd0c1a-44d3-4a7f-b647-9db8ac78b40d" -O "res/data/AUD-USD.zip"
-wget "https://drive.usercontent.google.com/download?id=1pgwQBoCFKCNDu5teR_DEQE0TPrUnmxly&export=download&authuser=0&confirm=t&uuid=853a604c-983e-4276-a0ab-26106b637f63&at=AN_67v0AJRo1LDn5N7qhwzps2X0d%3A1727326916450" -O "res/data/AUD-USD.zip"
+wget "https://drive.usercontent.google.com/download?id=1f6um7gYZzq5ipv8nhKnSGu7V7TuRT2qp&export=download&confirm=t&uuid=11a76e5c-37d1-468d-a429-0127a205f8ff" -O "res/data/AUD-USD.zip"
 unzip "res/data/AUD-USD.zip" -d "res/data/"
 
